6674dfcff8f4968f1bb80fb8421923edcdfe7a43
José Lopes <946393361bill@gmail.com>
2023-02-01 13:10:06 +0100

    getting user selected file

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
 const fileInput = document.querySelector(".file-input");
 const chooseImgBtn = document.querySelector(".choose-img");
 
+const loadImage = () => {
+    let file = fileInput.files[0] // gettinf user selected file
+    console.log(file)
+}
+
+fileInput.addEventListener("change", loadImage);
 chooseImgBtn.addEventListener("click", () => fileInput.click())
