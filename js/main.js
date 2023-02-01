01cfafc764711ac11ce82efbf7d467c961b93e41
José Lopes <946393361bill@gmail.com>
2023-02-01 15:19:59 +0100

    workin on slider range

--- js/main.js
+++ js/main.js
@@ -2,7 +2,11 @@ const fileInput = document.querySelector(".file-input");
 const previewImg = document.querySelector(".preview-img img");
 const chooseImgBtn = document.querySelector(".choose-img");
 
-const loadImage = (e) => {
+const filterOptions = document.querySelectorAll(".filter button");
+const filterName = document.querySelector(".filter-info .name");
+const filterSlider = document.querySelector(".slider input");
+
+const loadImage = () => {
     let file = fileInput.files[0] // getting user selected file
     if(!file) return; // para não retornar undefined em caso de não selecionar uma img  
     previewImg.src = URL.createObjectURL(file);
@@ -10,7 +14,20 @@ const loadImage = (e) => {
     previewImg.addEventListener("load", ()=> {
         document.querySelector(".container").classList.remove("disable");
     });
-}
+};
+
+filterOptions.forEach(option => {
+    option.addEventListener("click", ()=>{ // adding click event listenner to all filter buttons
+        document.querySelector(".filter .active").classList.remove("active");
+        option.classList.add("active");
+        filterName.innerText = option.innerText;
+    });
+});
+
+const updateFilter = () => {
+    console.log(filterSlider.value)
+};
 
 fileInput.addEventListener("change", loadImage);
-chooseImgBtn.addEventListener("click", () => fileInput.click())
+filterSlider.addEventListener("input", updateFilter);
+chooseImgBtn.addEventListener("click", () => fileInput.click());
